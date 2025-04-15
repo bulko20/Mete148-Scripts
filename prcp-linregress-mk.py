f158d3d6e0de8b54b90e25de807c22deddbaf8f2
camtono <165814320+bulko20@users.noreply.github.com>
2025-04-15 23:42:08 +0800

    Update prcp-linregress-mk.py
    Added "dpi" parameter

--- prcp-linregress-mk.py
+++ prcp-linregress-mk.py
@@ -49,7 +49,7 @@ years_table = np.arange(2040, 2101, 20)
 prcp_table = slope * years_table + intercept
 
 # Plotting the observed (with trend line) and projected precipitation
-plt.figure(figsize=(20, 10))
+plt.figure(figsize=(20, 10), dpi=900)
 plt.plot(years, precipitation, label='Observed Precipitation (1950-2024)', alpha=0.8, linewidth=2)
 plt.plot(years, slope * years + intercept, label='Trend Line', color='red', linestyle='--', linewidth=3)
 plt.plot(future_years, future_precipitation, label='Projected Precipitation', linestyle='--', color='green', linewidth=3)
